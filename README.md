64aa8f72d627cd4e3d8bbb1a9e9e304a1b918cee
justin sykes <justin.sykes@ge.com>
2020-09-23 15:54:30 -0400

    completely reworked script.js file & filled out README.md file

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # password-generator
+
+Webpage with a centered card that allows a user to generate a custom password using the following criteria.
+- 8 to 128 characters in length
+- user selections avaialbe for:
+    - uppercase letters
+    - lowercase letters
+    - numbers
+    - special characters
+
+URL of deployed app: https://204039643.github.io/02-responsive-portfolio/
+
+![screenshotofmywebpage](./Assets/Images/myPWgeneratorcreenshot1.png)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Clone my Github reposity to your local machine: https://github.com/204039643/password-generator
+
+## Usage
+
+1. Open local repository in VS code (code .).
+2. Navigate to the index.html, style.css, or script.js files in the editor to view.
+3. To view in your web browser, with the index.html file selecetd in the editor, right-click and select 'Open in default browser'.
+4. Click generate password button.
+5. Respond to prompts for user selection criteria.
+6. When complete with prompts, new password will populate in center of webpage card containter element.
+
+## Credits
+
+W3C Schools HTML semantics: https://www.w3schools.com/html/html5_semantic_elements.asp
+Bootstrap CDN: https://getbootstrap.com/docs/4.5/getting-started/introduction/
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Justin Sykes]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ Scripts/script.js
@@ -0,0 +1,76 @@
+//DOM variable
+//target 'generate' button element in UI
+var generateBtn = document.querySelector('#generate');
+
+//JS variables
+var lowercase = "abcdefghijkklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChar = "~!@#$%^&*()_+-";
+var possibleString = "";
+var password = "";
+var passwordText = "";
+
+//define functions
+function generatePassword() {
+
+    //ensure PW length is between 8 and 128 characters
+    var length = prompt("Please provide a password length (8 to 128 char.");
+    while (length < 8 || length > 128) {
+        var length = prompt("Please provide a password length (8 to 128 char.");
+        alert("Provide a length between 8 and 128 characters.");
+    };
+    console.log("PW length = " + length);
+
+    // begin user selection criteria for uppercase, lowercase, numbers, or special characters
+    var uppercaseChoice = confirm("Do you want to include uppercase letters?");
+    console.log("Uppercase = " + uppercaseChoice);
+    var lowercaseChoice = confirm("Do you want to include lowercase letters?");
+    console.log("Lowercase = " + lowercaseChoice);
+    var numbersChoice = confirm("Do you want to include lowercase letters?");
+    console.log("Numbers = " + numbersChoice);
+    var specialCharChoice = confirm("Do you want to include special characters?");
+    console.log("Special Characters = " + specialCharChoice);
+
+    //build possible PW string
+    if (uppercaseChoice) {
+        possibleString = possibleString + uppercase;
+    };
+    console.log(possibleString);
+
+    if (lowercaseChoice) {
+        possibleString = possibleString + lowercase;
+    };
+    console.log(possibleString);
+
+    if (numbersChoice) {
+        possibleString = possibleString + numbers;
+    };
+    console.log(possibleString);
+
+    if (specialCharChoice) {
+        possibleString = possibleString + specialChar;
+    };
+
+    //ensure at least one of the PW critera is selected to true
+    console.log(possibleString);
+    if (uppercaseChoice === false && lowercaseChoice === false && numbersChoice === false && specialCharChoice === false) {
+        alert("You must select at least one of the criteria to be 'true'.");
+        return;
+    }
+    //build password using random selection of characters from possibleString for user specified length
+    for (var i = 0; i < length; ++i) {
+        password += possibleString.charAt(Math.floor(Math.random() * possibleString.length));
+    };
+    console.log(password);
+}
+
+//write password back to #password element in UI
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
